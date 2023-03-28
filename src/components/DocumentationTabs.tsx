6ebe5b9e2f7f85bfeeaf1c47ae0a45bacc0bb405
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-03-28 19:36:34 +0600

    code component is almost done

--- src/components/DocumentationTabs.tsx
+++ src/components/DocumentationTabs.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/ui/Tabs";
+import SimpleBar from "simplebar-react";
 
 const DocumentationTabs: FC = ({}) => {
-  return <div>DocumentationTabs</div>;
+  return (
+    <Tabs defaultValue='nodejs' className='max-w-2xl w-full'>
+      <TabsList>
+        <TabsTrigger value='nodejs'>Node JS</TabsTrigger>
+        <TabsTrigger value='python'>Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value='nodejs'>
+        {/* <SimpleBar></SimpleBar> */}
+        <Code />
+      </TabsContent>
+      <TabsContent value='python'></TabsContent>
+    </Tabs>
+  );
 };
 
 export default DocumentationTabs;
