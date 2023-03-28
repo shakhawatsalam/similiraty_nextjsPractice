7a71244ecc4e25efa205de8d63d10040663ace18
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-03-28 11:07:09 +0600

    theme change is almost done

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,9 +1,11 @@
 "use client";
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { Moon, Sun } from "lucide-react";
+} from "@/components/ui/DropdownMenu";
+import { Laptop, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { FC } from "react";
 import Button from "./ui/Button";
@@ -18,10 +20,27 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
         <Button variant='ghost' size='sm'>
           <Sun className='rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100' />
           <Moon className='absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100' />
+          <span className='sr-only'>Toggle Theme</span>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent align='end' forceMount>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+                  <Sun className='mr-2 h-4 w-4' />
+                  <span>Light</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  <Moon className='mr-2 h-4 w-4' />
+                  <span>Dark</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+                  <Laptop className='mr-2 h-4 w-4' />
+                  <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
 
 export default ThemeToggle;
+
+
